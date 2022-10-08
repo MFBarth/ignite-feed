79dedd4efd56c78f5e3f171862b660220046ef04
Matheus Barth <matheusbarthf@gmail.com>
2022-10-08 16:59:31 -0300

    feat: dynamic post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 import './global.css';
-import { Sidebar } from './components/Sidebar';
-import { Post } from './components/Post';
 
-function App() {
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/mfbarth.png',
+      name: 'Matheus Barth',
+      role: 'Web Deleveloper'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-10-04 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/charles00willian.png',
+      name: 'Charles Willian',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-09-10 20:00:00'),
+  },
+];
+
+export function App() {
   return (
     <div>
       <Header />
 
       <div className={styles.wrapper}>
         <Sidebar />
-        
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            ))}
         </main>
       </div>
     </div>
   )
 }
-
-export default App
